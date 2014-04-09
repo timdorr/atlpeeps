0d73481ad56a7f925d0aebe5ec8d3825339e8b2b
timdorr <timdorr@timdorr.com>
2014-04-08 22:18:54 -0400

    Grab a large image from Facebook.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], image_size: 'large'
 end
