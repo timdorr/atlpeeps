419177e4f20939691234a8286861d2dca6edcd1e
timdorr <timdorr@timdorr.com>
2014-04-09 01:16:05 -0400

    Configure Twitter and Github providers.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], image_size: 'large'
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], { image_size: 'large' }
+  provider :twitter,  ENV['TWITTER_KEY'],  ENV['TWITTER_SECRET'],  { image_size: 'original' }
+  provider :github,   ENV['GITHUB_KEY'],   ENV['GITHUB_SECRET']
 end
