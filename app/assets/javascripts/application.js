b09f7ffde5b0e213e02828756dd09fa928076a35
timdorr <timdorr@timdorr.com>
2014-04-13 14:11:10 -0400

    Upload your own image.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,13 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require_tree .
+
+$("#upload-image").on("click", function(e){
+  e.preventDefault();
+
+  $("#upload-form").find(":file").trigger("click");
+});
+
+$("#upload-form").on("change", ":file", function(e){
+  $("#upload-form").trigger("submit");
+});
