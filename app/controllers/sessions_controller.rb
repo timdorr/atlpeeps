06d2f1d314efe413c847d118489cc8c1ce570680
timdorr <timdorr@timdorr.com>
2014-04-13 12:50:49 -0400

    Clean up alert messages.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,11 @@ class SessionsController < ApplicationController
 
     if signed_in?
       if @identity.user == current_user
-        redirect_to profile_path, notice: "You're already linked that account."
+        redirect_to profile_path, flash: { "alert-warning" => "You're already linked that account." }
       else
         @identity.user = current_user
         @identity.save!
-        redirect_to profile_path, flash: {success: "Connected to your #{@identity.provider_title} account."}
+        redirect_to profile_path, flash: { "alert-success" => "You've connected your #{@identity.provider_title} account."}
       end
     else
       if @identity.user.present?
@@ -22,18 +22,25 @@ class SessionsController < ApplicationController
         @identity.user = current_user
         @identity.save!
 
-        redirect_to profile_path, flash: {success: "You've successfully set up an ATLpeeps account. Now fill out your profile."}
+        redirect_to profile_path, flash: { "alert-success" => "You've successfully set up an ATLpeeps account. Now fill out your profile."}
       end
     end
   end
 
   def remove
-    current_user.identities.where(provider: params[:provider]).first.destroy
-    redirect_to profile_path
+    if current_user.identities.count == 1
+      return redirect_to profile_path, flash: { "alert-danger" => "You need to stay connected to at least one social account!" }
+    end
+
+    @identity = current_user.identities.where(provider: params[:provider]).first
+    title = @identity.provider_title
+    @identity.destroy
+
+    redirect_to profile_path, flash: { "alert-danger" => "You've disconnected your #{title} account."}
   end
 
   def destroy
     self.current_user = nil
-    redirect_to root_url, notice: "Signed out!"
+    redirect_to root_url, flash: { "alert-warning" => "Signed out!" }
   end
 end
