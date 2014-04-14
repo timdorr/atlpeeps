581390767c5ecd20ab6c607e0a7c7f00d1f0caca
timdorr <timdorr@timdorr.com>
2014-04-14 01:20:43 -0400

    Handle cross-domain linkage in the navbar.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def default_domain
+    Rails.env.production? ? "www.atlpeeps.net" : "atlpeeps.dev"
+  end
+
+  def atv_domain
+    Rails.env.production? ? "www.atvpeeps.net" : "atvpeeps.dev"
+  end
 end
