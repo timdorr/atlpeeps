2a0214643ee50e141fc5d10abb23af063bc5d0ee
timdorr <timdorr@timdorr.com>
2014-04-14 01:26:12 -0400

    Don't include www. It's added already.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def default_domain
-    Rails.env.production? ? "www.atlpeeps.net" : "atlpeeps.dev"
+    Rails.env.production? ? "atlpeeps.net" : "atlpeeps.dev"
   end
 
   def atv_domain
-    Rails.env.production? ? "www.atvpeeps.net" : "atvpeeps.dev"
+    Rails.env.production? ? "atvpeeps.net" : "atvpeeps.dev"
   end
 end
